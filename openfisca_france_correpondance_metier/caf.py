ab0112b1db32e6428f868cbe8f3d240519e6fa8c
Thomas Guillet <guillet.thomas@gmail.com>
2018-08-24 13:43:41 +0200

    Export first test for CAF

---
+++ openfisca_france_correpondance_metier/caf.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import caf_utils
+import csv
+import openfisca_france
+from datetime import datetime
+from openfisca_core.simulations import Simulation
+
+
+import sys
+
+from pprint import pprint
+
+filename = './doc/Exemple CAF.csv'
+
+if len(sys.argv) > 1:
+  filename = sys.argv[len(sys.argv)-1]
+
+legislation_francaise = openfisca_france.FranceTaxBenefitSystem()
+with open(filename, 'r') as file:
+  rows = csv.DictReader(file)
+  for data in rows:
+    period = datetime.strptime('2018-08-01', '%Y-%m-%d')
+    situation = caf_utils.createSituation(data, period)
+    pprint(situation)
+
+    simulation_actuelle = Simulation(tax_benefit_system=legislation_francaise, simulation_json=situation)
+    variable = 'af'
+
+    periodText = period.strftime('%Y-%m')
+    print('Cet exemple calcule la variable \'' + variable + '\' sur la période \'' + periodText + '\'.')
+    resultat = simulation_actuelle.calculate(variable, periodText)
+
+    print('Le résultat obtenu est ' + str(resultat) + '.')
+    print('')
